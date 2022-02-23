a5c3383fb2262df2175b09ffc73089400b3b2bd3
antek <antonimalinowski.kontakt@gmail.com>
2022-02-23 07:37:21 +0100

    Consider employee details for salary raise

--- src/main/java/com/antoni/employees/Main.java
+++ src/main/java/com/antoni/employees/Main.java
@@ -1,34 +1,65 @@
 package com.antoni.employees;
 
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String people = """
-            Flinstone, Fred, 1/1/1900, Programmer
-            Rubble, Barney, 2/2/1905, Manager
-            Flinstone, Wilma, 3/3/1910, Analyst
-            Rubble, Betty, 4/4/1915, CEO
+        String peopleText = """
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone2, Fred2, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
+            Flinstone3, Fred3, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
+            Flinstone4, Fred4, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=100}
+            Flinstone5, Fred5, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
+            Rubble2, Barney2, 2/2/1905, Manager, {orgSize=100,dr=4}
+            Rubble3, Barney3, 2/2/1905, Manager, {orgSize=200,dr=2}
+            Rubble4, Barney4, 2/2/1905, Manager, {orgSize=500,dr=8}
+            Rubble5, Barney5, 2/2/1905, Manager, {orgSize=175,dr=20}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
+            Flinstone2, Wilma2, 3/3/1910, Analyst, {projectCount=4}
+            Flinstone3, Wilma3, 3/3/1910, Analyst, {projectCount=5}
+            Flinstone4, Wilma4, 3/3/1910, Analyst, {projectCount=6}
+            Flinstone5, Wilma5, 3/3/1910, Analyst, {projectCount=9}
+            Rubble, Betty, 4/4/1915, CEO {avgStockPrice=300}
             """;
 
-        String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w*)\\n";
-        Pattern pat = Pattern.compile(regex);
-        Matcher mat = pat.matcher(people);
+        String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w*)(?:,\\s*\\{(?<details>.*)\\})?\\n";
+        Pattern peoplePat = Pattern.compile(peopleRegex);
+        Matcher peopleMat = peoplePat.matcher(peopleText);
+
+        String progRegex = "\\w+\\=(?<locpd>\\w+)\\,\\w+\\=(?<yoe>\\w+)\\,\\w+\\=(?<iq>\\w+)";
+        Pattern coderPat = Pattern.compile(progRegex);
 
         int totalSalaries = 0;
-        while (mat.find()) {
-            totalSalaries += switch (mat.group("role")) {
-                case "Programmer" -> 3000;
-                case "Manager" -> 3500;
-                case "Analyst" -> 2500;
-                case "CEO" -> 5000;
+        while (peopleMat.find()) {
+            totalSalaries += switch (peopleMat.group("role")) {
+                case "Programmer" -> {
+                    String details = peopleMat.group("details");
+                    Matcher coderMat = coderPat.matcher(details);
+                    int salary;
+                    if (coderMat.find()) {
+                        int locpd = Integer.parseInt(coderMat.group("locpd"));
+                        int yoe = Integer.parseInt(coderMat.group("yoe"));
+                        int iq = Integer.parseInt(coderMat.group("iq"));
+                        System.out.printf("Programmer loc: %s yoe: %s iq: %s%n", locpd, yoe, iq);
+                        salary = 3000 + locpd * yoe * iq;
+                    } else {
+                        salary = 3000;
+                    }
+                    yield salary;
+                }
+                case "Manager" -> {
+                    yield 3500;
+                }
+                case "Analyst" -> {
+                    yield 2500;
+                }
+                case "CEO" -> {
+                    yield 5000;
+                }
                 default -> 0;
             };
         }
-        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance(Locale.US);
-        System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
     }
 }
