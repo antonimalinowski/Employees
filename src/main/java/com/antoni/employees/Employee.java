5346df21cc343ef032a444b188fdaf888354aaa8
antek <antonimalinowski.kontakt@gmail.com>
2022-03-06 18:48:51 +0100

    Convert Employee interface into a class, introduce class hierarchy

--- src/main/java/com/antoni/employees/Employee.java
+++ src/main/java/com/antoni/employees/Employee.java
@@ -1,5 +1,37 @@
 package com.antoni.employees;
 
-public interface Employee {
-    int getSalary();
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Employee {
+    protected final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+    private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w*)(?:,\\s*\\{(?<details>.*)\\})?\\n";
+    protected final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+    protected final Pattern peoplePat = Pattern.compile(peopleRegex);
+    protected final Matcher peopleMat;
+    protected String lastName;
+    protected String firstName;
+    protected LocalDate dob;
+
+    public int getSalary() {
+        return 0;
+    }
+
+    public Employee(String personText) {
+        peopleMat = peoplePat.matcher(personText);
+        if (peopleMat.find()) {
+            this.lastName = peopleMat.group("lastName");
+            this.firstName = peopleMat.group("firstName");
+            this.dob = LocalDate.from(dtFormatter.parse(peopleMat.group("dob")));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
+    }
 }
