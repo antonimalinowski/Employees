cb81b60c7c42727d91c4bb4e449b56277616f47c
antek <antonimalinowski.kontakt@gmail.com>
2022-03-20 12:28:31 +0100

    Introduce default employee case, add factory method to create new employee for a matched role

--- src/main/java/com/antoni/employees/Employee.java
+++ src/main/java/com/antoni/employees/Employee.java
@@ -9,20 +9,43 @@ import java.util.regex.Pattern;
 
 public abstract class Employee {
     protected final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-    private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w*)(?:,\\s*\\{(?<details>.*)\\})?\\n";
+    private static final String PEOPLE_REGEX = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w*)(?:,\\s*\\{(?<details>.*)\\})?\\n";
     protected final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance(Locale.US);
-    protected final Pattern peoplePat = Pattern.compile(peopleRegex);
+    protected static final Pattern PEOPLE_PAT = Pattern.compile(PEOPLE_REGEX);
     protected final Matcher peopleMat;
     protected String lastName;
     protected String firstName;
     protected LocalDate dob;
 
-    public Employee(String personText) {
-        peopleMat = peoplePat.matcher(personText);
+    protected Employee() {
+        peopleMat = null;
+        lastName = "N/A";
+        firstName = "N/A";
+        dob = null;
+    }
+
+    protected Employee(String personText) {
+        peopleMat = Employee.PEOPLE_PAT.matcher(personText);
         if (peopleMat.find()) {
             this.lastName = peopleMat.group("lastName");
             this.firstName = peopleMat.group("firstName");
             this.dob = LocalDate.from(dtFormatter.parse(peopleMat.group("dob")));
+        }
+    }
+
+    // Flinstone5, Wilma5, 3/3/1910, Analyst, {projectCount=9}
+    public static final Employee createEmployee(String employeeText) {
+        Matcher peopleMat = Employee.PEOPLE_PAT.matcher(employeeText);
+        if (peopleMat.find()) {
+            return switch (peopleMat.group("role")) {
+                case "Programmer" -> new Programmer(employeeText);
+                case "Manager" -> new Manager(employeeText);
+                case "Analyst" -> new Analyst(employeeText);
+                case "CEO" -> new CEO(employeeText);
+                default -> new DefaultEmployee();
+            };
+        } else {
+            return new DefaultEmployee();
         }
     }
 
@@ -35,5 +58,12 @@ public abstract class Employee {
     @Override
     public String toString() {
         return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
+    }
+
+    private static final class DefaultEmployee extends Employee {
+        @Override
+        public int getSalary() {
+            return 0;
+        }
     }
 }
